a4a2a375446a0f8ce9272630b7e1d54a38d0b61b
asi_lukas <lukas.zagorsky@gmail.com>
2024-05-17 18:55:36 +0200

    add employee number with uniqueness check
    also refactor check for empty phone numbers

--- addons/GymbeamEmployee/models/employee.py
+++ addons/GymbeamEmployee/models/employee.py
@@ -2,11 +2,15 @@ from odoo import models, fields, api
 import base64
 import io
 import pandas
+from odoo.exceptions import ValidationError
 
 
 class Employee(models.Model):
     _inherit = "hr.employee"
 
+    employee_number = fields.Char(
+        string="Employee Number", track_visibility="onchange", required=True
+    )
     i_love_gb = fields.Boolean(string="I Love GymBeam", default=True)
     salary = fields.Integer(string="Salary")
     tax = fields.Integer(string="Tax")
@@ -40,14 +44,37 @@ class Employee(models.Model):
             "target": "new",
         }
 
+    # TODO main co delat priste, zajistit employee_number i na hr.applicant a aby byli unique
+    # bude se jmenovat applicant_number ale taky bude unique s employee_number
+    @api.constrains("employee_number")
+    def check_for_uniqueness(self):
+        if not self.employee_number:
+            return
+        if (
+            self.search_count(
+                [("id", "!=", self.id), ("employee_number", "=", self.employee_number)]
+            )
+            > 0
+        ):
+            # TODO maybe custom header
+            raise ValidationError("Employee Number must be unique!")
+
     @api.depends("salary", "tax")
     def _compute_total_salary(self):
         self.total_salary = self.salary + self.tax
 
+    def check_empty_field(self, vals, field, default_value) -> dict:
+        if vals.get(field) == False:
+            vals[field] = default_value
+        return vals
+
+    # TODO also replace mobile_phone??
     @api.model
     def write(self, vals):
-        # NOTE there needs to be explicit check for False
-        # NOTE the key can also be None when editing different fields
-        if vals.get("special_phone") == False:
-            vals["special_phone"] = "0901123456"
+        vals = self.check_empty_field(vals, "special_phone", "0901123456")
         return super(Employee, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        vals = self.check_empty_field(vals, "special_phone", "0901123456")
+        return super(Employee, self).create(vals)
