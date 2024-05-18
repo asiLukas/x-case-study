b3e9da9c7c15b85412af33b1e9f2c230935b48ff
asi_lukas <lukas.zagorsky@gmail.com>
2024-05-18 23:34:36 +0200

    add default to api_id

--- addons/GymbeamEmployee/models/job_position.py
+++ addons/GymbeamEmployee/models/job_position.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class JobPosition(models.Model):
     _inherit = "hr.job"
 
-    api_id = fields.Integer(string="API ID", required=True)
+    api_id = fields.Integer(string="API ID", required=True, default=0)
 
     _sql_constraints = [
         ("ref_unique", "unique(api_id)", "API ID should be unique!"),
